953c88bfcc22671b6aeffc729d947f72a70c2a4a
Carlos Escribano Rey <carlos@markhaus.com>
2013-07-19 11:33:24 +0200

    Trying Flask-Classy
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
 
+from random import choice
+
+from flask.ext.classy import FlaskView, route
+
 from dashboard import app
 
+quotes = [
+    "A noble spirit embiggens the smallest man! ~ Jebediah Springfield",
+    "If there is a way to do it better... find it. ~ Thomas Edison",
+    "No one knows what he can do till he tries. ~ Publilius Syrus"
+]
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+
+# http://pythonhosted.org/Flask-Classy/
+class QuotesView(FlaskView):
+    route_base = '/'
+
+    def index(self):
+        return "<br>".join(quotes)
+
+    def get(self, id):
+        id = int(id)
+        if id <= len(quotes):
+            return quotes[id - 1]
+        else:
+            return "Not Found", 404
+
+    @route('/word_bacon/')
+    def random(self):
+        return choice(quotes)
+
+QuotesView.register(app)
