b95f0523b7d89e859590e4b91df7db81f48aca31
Carlos Escribano Rey <carlos@markhaus.com>
2013-07-20 14:28:05 +0200

    Trying some libs
    Signed-off-by: Carlos Escribano Rey <carlos@markhaus.com>

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask.ext.bcrypt import Bcrypt
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
+app.config.from_pyfile('../cfg/settings.cfg')
+
+app_bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 import dashboard.views
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,31 +4,14 @@ from random import choice
 
 from flask.ext.classy import FlaskView, route
 
-from dashboard import app
-
-quotes = [
-    "A noble spirit embiggens the smallest man! ~ Jebediah Springfield",
-    "If there is a way to do it better... find it. ~ Thomas Edison",
-    "No one knows what he can do till he tries. ~ Publilius Syrus"
-]
+from dashboard import app, app_bcrypt, login_manager
 
 
-# http://pythonhosted.org/Flask-Classy/
-class QuotesView(FlaskView):
+class RootView(FlaskView):
     route_base = '/'
 
     def index(self):
-        return "<br>".join(quotes)
+        return ""
 
-    def get(self, id):
-        id = int(id)
-        if id <= len(quotes):
-            return quotes[id - 1]
-        else:
-            return "Not Found", 404
 
-    @route('/word_bacon/')
-    def random(self):
-        return choice(quotes)
-
-QuotesView.register(app)
+RootView.register(app)
